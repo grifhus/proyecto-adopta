4609ad2d12b1985b7a9c7f706b0c45cbce9eeb35
David Julian Jaramillo <davidjulianjaramillo@gmail.com>
2022-09-29 23:42:43 -0500

    port

--- app.js
+++ app.js
@@ -37,6 +37,6 @@ app.use(function(req,res,next){
 
 
 // Por donde va escuchar nuestro servidor
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("SERVER UP running in http://localhost:3000")
 })
